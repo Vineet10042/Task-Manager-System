96b9f8fd4d05d3c580beaeb95a32391a8e4e5ad4
Vineet10042 <puneetsingh7800@gmail.com>
2026-04-11 00:00:37 +0530

    server managed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,4 +5,5 @@ const { registerUser, loginUser } = require('../controllers/authController');
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 
+
 module.exports = router;
